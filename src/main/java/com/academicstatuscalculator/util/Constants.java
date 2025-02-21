4e41947803b6f5ead23a315025dce9050d29cc21
Christiano <christianodvids@gmail.com>
2025-02-21 20:50:27 -0300

    feat: add util folder and Constants file

---
+++ src/main/java/com/academicstatuscalculator/util/Constants.java
@@ -0,0 +1,10 @@
+package com.academicstatuscalculator.util;
+
+public class Constants {
+    public static final String APPLICATION_NAME = "Academic Status Calculator";
+    public static final String SPREADSHEET_ID = "1DP8of8h35jkWmjd1FGJ86VHAAQmdJ7T9QvkkBTpCCCU";
+    public static final String RANGE = "A1:H27";
+    public static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    public static final String TOKENS_DIRECTORY_PATH = "tokens";
+    public static final int TOTAL_CLASSES = 60;
+}
