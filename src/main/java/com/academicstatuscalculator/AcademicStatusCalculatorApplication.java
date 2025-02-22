a724da1ddbddcfe2d2056461b03224f0fbe7cf29
Christiano <christianodvids@gmail.com>
2025-02-21 21:46:04 -0300

    feat: update main file

--- src/main/java/com/academicstatuscalculator/AcademicStatusCalculatorApplication.java
+++ src/main/java/com/academicstatuscalculator/AcademicStatusCalculatorApplication.java
@@ -1,7 +1,16 @@
 package com.academicstatuscalculator;
 
-public class AcademicStatusCalculatorApplication {
-    public static void main(String[] args) {
+import com.academicstatuscalculator.controller.StudentController;
+import com.academicstatuscalculator.view.StudentView;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class AcademicStatusCalculatorApplication {
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(view);
+
+        controller.processStudentData();
     }
 }
