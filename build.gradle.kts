b78a9092c6c232252b2837514fb9225d607f6479
Christiano <christianodvids@gmail.com>
2025-02-22 17:21:14 -0300

    fix: fixed JUnit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,9 @@ dependencies {
     implementation("com.google.api-client:google-api-client:2.0.0")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
