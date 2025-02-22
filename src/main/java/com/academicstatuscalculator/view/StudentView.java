20f411c7a8f1c746491d80eca220f28aa91d2815
Christiano <christianodvids@gmail.com>
2025-02-21 21:16:42 -0300

    feat: add printHeader() in view file

--- src/main/java/com/academicstatuscalculator/view/StudentView.java
+++ src/main/java/com/academicstatuscalculator/view/StudentView.java
@@ -1,7 +1,15 @@
 package com.academicstatuscalculator.view;
 
+import java.util.List;
+
 public class StudentView {
     public void printNoDataFound() {
         System.out.println("No data found.");
+    }
+
+    public void printHeader(List<Object> headerRow) {
+        System.out.printf("%n|%-10s|%-14s|%-8s|%-5s|%-5s|%-5s|%-5s|%-25s|%-25s|%n",
+                headerRow.get(0), headerRow.get(1), headerRow.get(2), headerRow.get(3),
+                headerRow.get(4), headerRow.get(5), "Media", headerRow.get(6), headerRow.get(7));
     }
 }
