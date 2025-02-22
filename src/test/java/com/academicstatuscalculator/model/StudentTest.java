efdf70cfe85f699621b393bb62a558192a6fb30a
Christiano <christianodvids@gmail.com>
2025-02-22 13:35:46 -0300

    feat: add testStudentFinalGradeForApproval() in test file

--- src/test/java/com/academicstatuscalculator/model/StudentTest.java
+++ src/test/java/com/academicstatuscalculator/model/StudentTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StudentTest {
     @Test
     public void testStudentGettersAndSetters() {
-        Student student = new Student("30", "Fernanda Lima", 2, 85, 88, 92);
+        Student student = new Student("30", "Marina Carvalho", 2, 85, 88, 92);
 
         student.setAverage(8.8);
         student.setResult("Aprovado");
@@ -27,5 +27,18 @@ public class StudentTest {
 
         assertEquals(7.0, student.getAverage());
         assertEquals("Aprovado", student.getResult());
+    }
+
+    @Test
+    public void testStudentFinalGradeForApproval() {
+        Student student = new Student("32", "Natalia Souza", 4, 55, 60, 65);
+
+        student.setAverage(6.0);
+        student.setResult("Exame Final");
+        student.setFinalGradeForApproval(4.0);
+
+        assertEquals(6.0, student.getAverage());
+        assertEquals("Exame Final", student.getResult());
+        assertEquals(4.0, student.getFinalGradeForApproval());
     }
 }
