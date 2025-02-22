b77b38e32d96ad03656206edf841b9141c501d46
Christiano <christianodvids@gmail.com>
2025-02-21 21:06:46 -0300

    feat: add controller folder and StudentController file

---
+++ src/main/java/com/academicstatuscalculator/controller/StudentController.java
@@ -0,0 +1,11 @@
+package com.academicstatuscalculator.controller;
+
+import com.academicstatuscalculator.view.StudentView;
+
+public class StudentController {
+    private final StudentView view;
+
+    public StudentController(StudentView view) {
+        this.view = view;
+    }
+}
