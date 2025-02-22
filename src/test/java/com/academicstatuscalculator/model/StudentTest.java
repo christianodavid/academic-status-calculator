58e2d98f0f3c46f35fb0769aa5cbace6690d0cfe
Christiano <christianodvids@gmail.com>
2025-02-22 13:26:01 -0300

    feat: add testStudentGettersAndSetters() in test file

--- src/test/java/com/academicstatuscalculator/model/StudentTest.java
+++ src/test/java/com/academicstatuscalculator/model/StudentTest.java
@@ -1,4 +1,20 @@
 package com.academicstatuscalculator.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StudentTest {
+    @Test
+    public void testStudentGettersAndSetters() {
+        Student student = new Student("30", "Fernanda Lima", 2, 85, 88, 92);
+
+        student.setAverage(8.8);
+        student.setResult("Aprovado");
+        student.setFinalGradeForApproval(0);
+
+        assertEquals(8.8, student.getAverage());
+        assertEquals("Aprovado", student.getResult());
+        assertEquals(0.0, student.getFinalGradeForApproval());
+    }
 }
